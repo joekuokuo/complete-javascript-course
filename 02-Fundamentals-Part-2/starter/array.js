1f6c84779541d7361787d61101b5fdb9999b1060
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-24 14:44:48 -0800

    Lec 39 add array.js

---
+++ 02-Fundamentals-Part-2/starter/array.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const friends = ["Joe", "Ken", "Amy"]
+console.log(friends[0]);
+console.log(friends[friends.length - 1]);
+
+
+const year = new Array(2000, 2010, 2020);
+console.log(year);
+
+// year = [123, 321] // illegal
+

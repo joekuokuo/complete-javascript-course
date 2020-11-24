ac9ca875efa1350e920b251d0ef18be8b2fd08e1
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-24 09:23:11 -0800

    Lec 32 strict mode.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict'; //activate the strict mode
+
+let isTrue = false;
+const t = true;
+if (t) istrue = true; // w/o the strict mode the error was handled silently
+
+if (isTrue) console.log("Is True.");
+
+
+// reserve words
+// const interface = "TV"; // error interface is a reserve word.
+// const provate = 12; // error interface is a reserve word.
+

3c5e498516097253e1d709f40b845bf0b218b6d0
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-23 20:51:54 -0800

    lec20 type coversion and coercion

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,83 @@
+// let js = 'amazing';
+
+// console.log(1 + 2);
+
+// console.log(12)
+
+// console.log(js);
+
+// // ======================================
+
+// let jsIsFun = true;
+// console.log(jsIsFun);
+// console.log(typeof jsIsFun);
+
+// console.log(typeof null); // print object which is a legacy bug
+
+
+// // ======================================
+
+// const now = 2020;
+// const ageJoe = now - 1993;
+// const ageMay = now - 2000;
+
+// console.log(ageJoe, ageMay);
+// console.log(3 ** 2);
+
+// const firstName = "Joe"
+// const lastName = "Kuo"
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5; // 15
+// x += 10;
+// x++; //26
+// x--;
+// console.log(x);
+
+// console.log(ageJoe > ageMay);
+// console.log(ageMay >= 18);
+
+// const firstName = "Joe"
+// const lastName = "Kuo"
+// const birth = 1993;
+// const year = 2020;
+// const job = "student"
+
+// ======================================
+
+// const joe = "I'm " + firstName + ", a " + (year - birth) + " year old " + job + " !";
+// console.log(joe);
+
+// const joeNew = `I'm ${firstName}, a ${year - birth} year old${job}`;
+// console.log(joeNew);
+
+// console.log(`These are 
+// multilines`);
+
+// ======================================
+
+// const age = 14;
+// const isValid = age >= 18;
+
+// if (isValid) {
+//     console.log("Joe can drive.")
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log(`Joe needs to wait another ${yearLeft} years`);
+// }
+
+// ======================================
+
+// type conversion
+// const year = "1999";
+// console.log(Number(year) + 19);
+
+// // type coercion
+// console.log("I'm " + 23 + " years old");
+// console.log('12' - '12') // substract triggers coercion
+// console.log('24' * '2'); // triggers coercion
+// console.log('24' / '2'); // triggers coercion
+// console.log('23' > '19'); // triggers coercion
+
+
+

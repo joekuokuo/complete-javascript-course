a5207ce5c5a0faf732e4ad7365467d1500b3ca73
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-24 22:23:36 -0800

    Lec 47 add loop.js

---
+++ 02-Fundamentals-Part-2/starter/loop.js
@@ -0,0 +1,27 @@
+'use strict';
+
+for (let i = 1; i <= 10; i += 2) {
+    console.log(`Count: ` + i);
+}
+
+const friends = ["Joe", "Ken", "Amy", 12, [123, 123]];
+const types = []
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+
+    // Filling the elements to the array
+    // types.push(typeof friends[i]);
+    types[i] = typeof friends[i];
+}
+
+console.log(types);
+
+
+// continue and break
+for (let i = 0; i < friends.length; i++) {
+    if (typeof friends[i] !== 'string') continue;
+    // else if (typeof friends[i] === 'object') break;
+    // types[i] = typeof friends[i];
+    console.log(typeof friends[i]);
+}

0105f1c63db609eca1f508ca106a74c200518cee
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-26 15:41:12 -0800

    Lec 72 basic DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//DOM manipulation
+// select class message
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number :)';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 13;
+console.log(document.querySelector('.guess').value);

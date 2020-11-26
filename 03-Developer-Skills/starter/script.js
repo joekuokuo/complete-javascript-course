521553ead8ec28e213593e9a006cb1d4ae9d6590
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-25 22:43:02 -0800

    Finish until Section 6

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '123';
+// debugger; The browser will automatically show the debugger
+const y = we => we;
+console.log('Hello');

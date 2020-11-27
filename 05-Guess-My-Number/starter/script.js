9808a0706f48a7bd7bbb6d24f991ae12b6928f36
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-26 16:08:46 -0800

    Lec 73 Choose the input by DOM manipulation and add some logic to it.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,12 +2,21 @@
 
 //DOM manipulation
 // select class message
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number :)';
+// document.querySelector('.message').textContent = 'Correct Number :)';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 13;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 13;
+// console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); // to convert a string to number
+  console.log(guess, typeof guess);
+
+  // To make sure there is a number for the input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});

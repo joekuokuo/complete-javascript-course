1350333a1cf74475745bb40b74af894e4d6fb7fc
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-27 12:08:32 -0800

    Lec 80 Open and close modal using button and clicking on overlay area.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal'); // create a list for all selected Modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  //   console.log(btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener('click', function () {
+    // console.log('Button clicked'); //test
+
+    // usually we add and remove the hidden class to
+    modal.classList.remove('hidden'); // don't use ".hidden"
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);

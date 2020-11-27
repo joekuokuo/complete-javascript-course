fda5b548e96c82cd05a74e3efd431622a038bec8
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-11-27 14:40:58 -0800

    Finish Modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -12,8 +12,6 @@ const closeModal = function () {
 for (let i = 0; i < btnShowModal.length; i++) {
   //   console.log(btnShowModal[i].textContent);
   btnShowModal[i].addEventListener('click', function () {
-    // console.log('Button clicked'); //test
-
     // usually we add and remove the hidden class to
     modal.classList.remove('hidden'); // don't use ".hidden"
     overlay.classList.remove('hidden');
@@ -22,3 +20,18 @@ for (let i = 0; i < btnShowModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// Use esc key press to close the modal
+// key event is a global event, so add the event on the document itself
+// keydown, keypress, keyup
+// Need to look at the event itself to find out which key is pressed
+document.addEventListener('keydown', function (e) {
+  //   console.log(e.key);
+  if (!modal.classList.contains('hidden') && e.key === 'Escape') {
+    closeModal();
+    // console.log('Esc pressed');
+    // if (!modal.classList.contains('hidden')) {
+    //   closeModal();
+    // }
+  }
+});

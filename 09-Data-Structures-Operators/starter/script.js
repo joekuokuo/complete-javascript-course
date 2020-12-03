99f91c32ebc1b8a38ab132c84f23dfdfb2488baf
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-12-02 19:09:15 -0800

    Lec 112 optional chaining object, method, and arrays.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -86,7 +86,32 @@ const restaurant = {
   },
 };
 
-console.log(restaurant.order(1, 0));
+// console.log(restaurant.order(1, 0));
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// ES2020+ optional chaining & Nullish coalescing introduced
+// With optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  // console.log(day);
+  // const open = restaurant.openingHours[day]?.open || 'closed'; // Cause an error that the open ar sat is 0 which is a falsy value
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Method
+console.log(restaurant.order?.(0, 1) ?? 'Method not exist!');
+console.log(restaurant.orderHam?.(0, 1) ?? 'Method not exist!'); // This print not exist
+
+// Arrays
+const users = [{ name: 'Joe', email: 'ioqweiqwoie@gmail.com', age: 29 }];
+console.log(users[0]?.name ?? 'User array empty');
+console.log(users[1]?.name ?? "User doesn't exist");
 
 /*
 ////////////////////////////////////////////////

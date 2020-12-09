29a34ca581d0c965b8d08f1677ecdd13bf02c53a
Tsung-Da Kuo <tsungdakuo@gmail.com>
2020-12-08 22:37:32 -0800

    Lec 126 functions - default values assign

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+const bookings = [];
+
+const createBooking = function (
+  flightNum = 'XXX',
+  numPessengers = 1,
+  price = 100 * numPessengers
+) {
+  // default value
+  // ES5
+  //   flightNum = 1;
+  //   numPessengers = 1;
+  //   price = 100;
+
+  const booking = {
+    flightNum,
+    numPessengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('L123', 2, 100);
+createBooking('L123', 2);
+createBooking('L123', undefined, 400); // use undefined to skip parameters
